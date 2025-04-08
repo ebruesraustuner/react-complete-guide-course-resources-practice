9baeed53ee6d8a3743c264567ab64339fffb7977
Ebruesra <ebruesra06@gmail.com>
2025-04-08 07:18:24 +0000

    feat: form actions

---
+++ Learnings/17b Form Actions/src/components/Signup.jsx
@@ -0,0 +1,134 @@
+import { isEmail, isNotEmpty, hasMinLength } from '../util/validation.js';
+import { useActionState } from 'react';
+function submitAction(prevFormState, formData){
+  console.log("🚀 ~ submitAction ~ prevFormState:", prevFormState)
+  const email = formData.get('email');
+  const password = formData.get('password');
+  const confirmPassword = formData.get('confirmPassword');
+
+let errors = [];
+if(!isEmail(email)) {
+  errors.push('Invalid email')
+}
+
+if(!isNotEmpty(password) || !hasMinLength(password, 6)) {
+  errors.push('something wrong on pass')
+}
+
+if(errors.length > 0) {
+  return { errors, enteredValues: {
+    email,
+    password,
+    confirmPassword
+  }}
+}
+
+return { errors: null}
+}
+
+
+export default function Signup() {
+  
+  const [currFormState, currFromAction] =useActionState(submitAction, {errors: null});
+
+  return (
+    <form action={currFromAction}>
+      <h2>Welcome on board!</h2>
+      <p>We just need a little bit of data from you to get you started 🚀</p>
+
+      <div className="control">
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" name="email" defaultValue={currFormState.enteredValues?.email}/>
+      </div>
+
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" defaultValue={currFormState.enteredValues?.password} />
+        </div>
+
+        <div className="control">
+          <label htmlFor="confirm-password">Confirm Password</label>
+          <input
+            id="confirm-password"
+            type="password"
+            name="confirm-password"
+            defaultValue={currFormState.enteredValues?.confirmPassword}
+          />
+        </div>
+      </div>
+
+      <hr />
+
+      <div className="control-row">
+        <div className="control">
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" name="first-name" />
+        </div>
+
+        <div className="control">
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" name="last-name" />
+        </div>
+      </div>
+
+      <div className="control">
+        <label htmlFor="phone">What best describes your role?</label>
+        <select id="role" name="role">
+          <option value="student">Student</option>
+          <option value="teacher">Teacher</option>
+          <option value="employee">Employee</option>
+          <option value="founder">Founder</option>
+          <option value="other">Other</option>
+        </select>
+      </div>
+
+      <fieldset>
+        <legend>How did you find us?</legend>
+        <div className="control">
+          <input
+            type="checkbox"
+            id="google"
+            name="acquisition"
+            value="google"
+          />
+          <label htmlFor="google">Google</label>
+        </div>
+
+        <div className="control">
+          <input
+            type="checkbox"
+            id="friend"
+            name="acquisition"
+            value="friend"
+          />
+          <label htmlFor="friend">Referred by friend</label>
+        </div>
+
+        <div className="control">
+          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <label htmlFor="other">Other</label>
+        </div>
+      </fieldset>
+
+      <div className="control">
+        <label htmlFor="terms-and-conditions">
+          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          agree to the terms and conditions
+        </label>
+      </div>
+
+
+    {currFormState.errors && 
+    ( <ul className='errors'> {currFormState.errors.map((e) => (
+        <li key={e}>{e}</li>
+    ))} </ul>)}
+      <p className="form-actions">
+        <button type="reset" className="button button-flat">
+          Reset
+        </button>
+        <button className="button">Sign up</button>
+      </p>
+    </form>
+  );
+}
